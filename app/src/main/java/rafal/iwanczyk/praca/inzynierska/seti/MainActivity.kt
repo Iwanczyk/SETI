e7b8458b3601cfb8400f04392bd1fd9548f3c21c
Iwanczyk <61409101+Iwanczyk@users.noreply.github.com>
2022-11-01 20:58:18 +0100

    Initial commit

---
+++ app/src/main/java/rafal/iwanczyk/praca/inzynierska/seti/MainActivity.kt
@@ -0,0 +1,11 @@
+package rafal.iwanczyk.praca.inzynierska.seti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
